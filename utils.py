9311e4506dbd74037b5ee401360e3fc343fc2445
bliuag <bliuag@connect.ust.hk>
2019-04-28 15:43:33 -0400

    add bounding box generation

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 from PIL import Image
 import time
+from skimage import measure
+
 def load_image(filename):
     img = Image.open(filename)
     return np.array(img).transpose(2,0,1)
@@ -9,6 +11,25 @@ def time_for_file(localtime=True):
     ISOTIMEFORMAT='%d-%h-at-%H-%M-%S'
     f = time.localtime if localtime else time.gmtime
     return '{}'.format(time.strftime(ISOTIMEFORMAT, f(time.time())))
+
+def output_bbox(heatmap, threshold):
+    h, w, c = heatmap.shape
+    blobs = heatmap > threshold
+    bbox = []
+    for i in range(c):
+        heatmap_c = blobs[:,:,i]
+        blobs_labels, num_labels = measure.label(heatmap_c, background=0, connectivity=2, return_num=True)
+
+        for j in range(1, num_labels+1):
+            object_j = (blobs_labels==j)
+            row_index = np.where(np.max(object_j, axis=1))
+            left = np.min(row_index)
+            right = np.max(row_index)
+            col_index = np.where(np.max(object_j, axis=1))
+            top = np.min(col_index)
+            bottom = np.max(col_index)
+            bbox.append(np.array([left, right, top, bottom, i]))
+    return np.array(bbox)
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -27,4 +48,3 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
-
