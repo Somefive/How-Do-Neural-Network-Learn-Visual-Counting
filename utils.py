f5a30c10a7dfd66dd983f33af9e32699fe8c165a
bliuag <bliuag@connect.ust.hk>
2019-04-28 16:00:09 -0400

    fix bounding box

--- utils.py
+++ utils.py
@@ -17,18 +17,21 @@ def output_bbox(heatmap, threshold):
     blobs = heatmap > threshold
     bbox = []
     for i in range(c):
-        heatmap_c = blobs[:,:,i]
-        blobs_labels, num_labels = measure.label(heatmap_c, background=0, connectivity=2, return_num=True)
+        bbox_c = []
+        blob_c = blobs[:,:,i]
+        blobs_labels, num_labels = measure.label(blob_c, background=0, connectivity=2, return_num=True)
 
         for j in range(1, num_labels+1):
             object_j = (blobs_labels==j)
-            row_index = np.where(np.max(object_j, axis=1))
+            row_index = np.where(np.max(object_j, axis=0))
             left = np.min(row_index)
             right = np.max(row_index)
             col_index = np.where(np.max(object_j, axis=1))
             top = np.min(col_index)
             bottom = np.max(col_index)
-            bbox.append(np.array([left, right, top, bottom, i]))
+            bbox_c.append(np.array([left, right, top, bottom, np.mean(heatmap[:,:,i][object_j])]))
+
+        bbox.append(np.array(bbox_c))
     return np.array(bbox)
 
 class AverageMeter(object):
